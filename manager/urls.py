e53b3bcbfb11389abc70db95a2cabb808a0c41d5
Denys Halytskyi <denys.halytskyi17@gmail.com>
2026-04-11 20:15:01 +0300

    implement index view with basic statistics and configure home url

--- manager/urls.py
+++ manager/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+
+from manager.views import index
+
+app_name = "manager"
+urlpatterns = [
+    path("home/", index, name="index"),
+]
--- manager/views.py
+++ manager/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+from manager.models import Task, Worker
+
+
+def index(request: HttpRequest) -> HttpResponse:
+    num_tasks = Task.objects.all().count()
+    num_workers = Worker.objects.all().count()
+    context = {
+        "num_tasks": num_tasks,
+        "num_workers": num_workers,
+    }
+    return render(request, "manager/index.html", context=context)
