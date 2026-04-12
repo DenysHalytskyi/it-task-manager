8135942fa2c244b146384b11c55b3acbc86a6aa8
Denys Halytskyi <denys.halytskyi17@gmail.com>
2026-04-12 17:19:36 +0300

    replace sidebar placeholders with real routes and set up home page url

--- manager/urls.py
+++ manager/urls.py
@@ -4,7 +4,7 @@ from manager.views import index, WorkerListView, TaskListView, PositionListView,
 
 app_name = "manager"
 urlpatterns = [
-    path("home/", index, name="index"),
+    path("", index, name="index"),
     path("workers/", WorkerListView.as_view(), name="worker-list"),
     path("tasks/", TaskListView.as_view(), name="task-list"),
     path("positions/", PositionListView.as_view(), name="position-list"),
